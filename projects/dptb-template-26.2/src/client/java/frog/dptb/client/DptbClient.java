3d11d9d3a00f9b8ef6217b416eff3d63d7e9c648
John Doe <johndoe@example.com>
2026-07-25 02:06:04 -0400

    setting up mod

---
+++ projects/dptb-template-26.2/src/client/java/frog/dptb/client/DptbClient.java
@@ -0,0 +1,10 @@
+package frog.dptb.client;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class DptbClient implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
